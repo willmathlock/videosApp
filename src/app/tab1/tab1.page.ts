526994734494d548866da58aec35a56fdf7d4429
William Matheus <wgtlwill@gmail.com>
2022-05-12 14:58:42 -0300

    AlertBox tab filmes ao favoritar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Deseja <strong>favoritar</strong> o filme!?',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
